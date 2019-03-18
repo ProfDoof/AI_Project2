619ad781ed8bc03d9df78a730390c9944a71cbdf
Brighton <43894829+bdm15b@users.noreply.github.com>
2019-03-18 18:13:43 -0500

    Add files via upload
    sorted freq now

--- Frequency/Frequency_Gen.py
+++ Frequency/Frequency_Gen.py
@@ -1,68 +1,69 @@
-import string
-
-files = ["walden.txt","david_copperfield.txt","pride_and_prejudice.txt","shakespeare.txt"]
-# files = ["abc.txt"]
+files = ["walden.txt","david_copperfield.txt","pride_and_prejudice.txt", "shakespeare.txt"]
 
 def main():
 
     freq_1 = {}
     freq_2 = {}
     freq_3 = {}
-    tot1 = 0
-    tot2 = 0
-    tot3 = 0
 
     for filename in files:
 
         # open file
-        file = open(filename, "r")
+        file = open(filename, "r", encoding="utf-8")
 
         # strip all nonalphabetic characters from string
-        file_contents = "".join([char.upper() for char in "".join((file.read()).split()) if char.isalpha()])
+        file_contents = "".join([char.upper() for char in "".join((file.read()).split()) if char in "abcdefghihjklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"])
         file.close()
-        # print(file_contents)
-
+        
         # generate sub-strings of sizes (1,2,3)
+        file_contents = list(file_contents[i:k+1] for i in range(len(file_contents)) for k in range(i, i + 3) if k < len(file_contents))
+
         # iterate through sub-strings and add frequencies to dictionaries
-        for i in range( len(file_contents) ):
-            tot1 += 1
-            sub_str = file_contents[i]
-            if sub_str not in freq_1:
-                freq_1[sub_str] = 1.0
+        for string in file_contents:
+            if len(string) == 1:
+                if string not in freq_1:
+                    freq_1[string] = 1.0
+                else:
+                    freq_1[string] += 1.0
+
+            elif len(string) == 2:
+                if string not in freq_2:
+                    freq_2[string] = 1.0
+                else:
+                    freq_2[string] += 1.0
+
             else:
-                freq_1[sub_str] += 1.0
-
-            if i > 0:
-                tot2 += 1
-                sub_str = file_contents[i-1:i+1]
-                if sub_str not in freq_2:
-                    freq_2[sub_str] = 1.0
+                if string not in freq_3:
+                    freq_3[string] = 1.0
                 else:
-                    freq_2[sub_str] += 1.0
-
-            if i > 1:
-                tot3 += 1
-                sub_str = file_contents[i-2:i+1]
-                if sub_str not in freq_3:
-                    freq_3[sub_str] = 1.0
-                else:
-                    freq_3[sub_str] += 1.0
-
+                    freq_3[string] += 1.0
+        
         print(filename, "complete")
 
-
+    
+    # write results to file
     file = open("Frequency.txt", "w")
-    for k, v in freq_1.items():
-        v_output = "%.9f" % (v/tot1)
-        file.write(k + " " + v_output + "\n")
+    for k, v in sorted(freq_1.items(), reverse = True, key = lambda x : x[1]):
+        file.write(k + " " + str(v / sum(freq_1.values())) + "\n")
+    #file.close()
 
-    for k, v in freq_2.items():
-        v_output = "%.9f" % (v/tot2)
-        file.write(k + " " + v_output + "\n")
+    #file = open("Frequency_.txt", "w")
+    i = 0
+    for k, v in sorted(freq_2.items(), reverse = True, key = lambda x : x[1]):
+        file.write(k + " " + str(v / sum(freq_2.values())) + "\n")
+        if i == 56:
+            break
+        i += 1
+    #file.close()
 
-    for k, v in freq_3.items():
-        v_output = "%.9f" % (v/tot3)
-        file.write(k + " " + v_output + "\n")
+    i = 0
+    #file = open("Frequency_3.txt", "w")
+    for k, v in sorted(freq_3.items(), reverse = True, key = lambda x : x[1]):
+        file.write(k + " " + str(v / sum(freq_3.values())) + "\n")
+        if i == 114:
+            break
+        i += 1
+    #file.close()
 
     file.close()
 
