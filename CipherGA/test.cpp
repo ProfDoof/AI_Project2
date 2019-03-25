0fd2afecc43a114a0a9bc9e6698592e2f41c5214
John Marsden <jmm15f@acu.edu>
2019-03-25 13:46:29 -0500

    Testing to see convergence rate.

--- CipherGA/test.cpp
+++ CipherGA/test.cpp
@@ -3,6 +3,16 @@
 
 int main()
 {
-    GACipher test(.1,.1,60000,500);
+    double copyRate = .1;
+    double mutationRate = .1;
+    int popSize = 100;
+    double minutes = 60;
+    minutes *= 60000;
+    double uniWeight = 1;
+    double diWeight = 1;
+    double triWeight = 1;
+    std::string actualKey = "NVPJUEGRAQZCLKBWYDOHFISXTM";
+    GACipher test(copyRate,mutationRate,minutes,popSize,actualKey);
+    test.setFreqWeights(uniWeight,diWeight,triWeight);
     test.run("../Test_Files/Cipher/test_1.txt");
 }
